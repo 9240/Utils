9e163d6568fac1e4e27efa75247a23436d2539ca
9240 <992027995@qq.com>
2020-08-29 23:10:48 +0800

    大整数相加

--- Utils.js
+++ Utils.js
@@ -1,8 +1,9 @@
 var zw = {}
-/*
-*	json转tree
-*	param：{data:json数据，parentId:父Id}
-*/
+/**
+ * json转tree
+ * @param {Object} data 
+ * @param {Number} parentId 
+ */
 zw.json2tree = function(data, parentId) {
 	let tree = [];
 	for (let i = 0; i < data.length; i++) {
@@ -12,7 +13,7 @@ zw.json2tree = function(data, parentId) {
 			treeItem.name = node.name;
 			treeItem.fatherid = node.fatherid;
 			treeItem.userid = node.userid;
-			treeItem.nodes = json2tree(data, node.userid);
+			treeItem.nodes = this.json2tree(data, node.userid);
 			tree.push(treeItem);
 		}
 	}
@@ -26,7 +27,7 @@ zw.deepCopy = function(from,to={}){
 	for(let key in from){
 		if(typeof from[key] === 'object'){
 			temp[key] = new (from[key].constructor)()
-			zw.deepCopy(from[key],temp[key])
+			this.deepCopy(from[key],temp[key])
 		}else{
 			temp[key] = from[key]
 		}
@@ -51,8 +52,36 @@ zw.jsonp = function({url,params,cb}){
 		for (const key in params) {
 			arr.push(`${key}=${params[key]}`)
 		}
-		
 		script.src = `${url}?${arr.join('&')}`
 		document.body.appendChild(script)
 	})
+}
+/**
+ * 大整数相加
+ * @param {Number} num1 
+ * @param {Number} num2 
+ */
+zw.bigAdd = function(num1,num2){
+	var numArr1 = [...(''+num1)].reverse()
+	var numArr2 = [...(''+num2)].reverse()
+	var Arr = []
+	var newArr = []
+	var carry = 0
+	for (let i = 0; i < Math.max(numArr1.length, numArr2.length); i++) {
+		n1 = numArr1[i] || 0
+		n2 = numArr2[i] || 0
+		Arr.push(Number(n1) + Number(n2))
+	}
+	for (let j = 0; j < Arr.length; j++) {
+		if((Arr[j]+carry)>9 && j != Arr.length-1){
+			newArr.push(Number(String(Arr[j]+carry)[1]))
+			carry = Number(String(Arr[j]+carry)[0])
+		}else if(j == Arr.length-1){
+			newArr.push(Arr[j]+carry)
+		}else{
+			newArr.push(Arr[j])
+			carry = 0
+		}
+	}
+	return newArr.reverse().join('')
 }
